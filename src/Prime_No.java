e041d3bf539cb578a52e50a878614e4b57ba2523
Swamiji <amitroy314@gmail.com>
2017-03-15 12:34:44 +0530

    Calender
    Calender

---
+++ src/Prime_No.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Prime_No {
+public static void main(String[] args) {
+	System.out.println("Enter the all elements :");
+	Scanner s=new Scanner(System.in);
+	int a[]=new int[10];
+	for(int i=0;i<10;i++)
+	{
+		a[i]=s.nextInt();
+	}
+	for(int i=0;i<a.length;i++)
+	{
+		boolean flag=true;
+		for(int j=2;j<i;j++)
+		{
+			if(a[i]%j==0)
+			{
+				flag=false;
+				break;
+			}
+			if(flag==true)
+			{
+				System.out.print("Prime_No :");
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
+}
+
+
+	
+
+	
+
+
+
